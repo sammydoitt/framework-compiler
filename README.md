3a412d06449e15fe4af22cd35f7a9a1e5510adf3
Sammy Steiner <sasteiner@doitt.nyc.gov>
2019-08-08 16:50:57 -0400

    working form for color hex codes.

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ The purpose of this application is to create a publicly accessible tool for down
 1. clone the repository to a local directory using the ```git clone``` command.
 2. cd into the repository directroy and run ```npm install```.
 3. run ```set DEBUG=myapp:* & npm start``` to start the server.
-4. the application will run on localhost:3000, and the current routes are "/" for the index, and "/compiler" to compiler and view the compiled css
+4. the application will run on localhost:3000, and the current routes are "/" for the index, and "/compiler" to reach the color customization form.
+5. On the form, enter valid color hex codes and submit the form to recieve the compiled CSS in the browser.
 
 ## Roadmap
-1. Color Customization for "primary," "secondary," "info," "warning," and "danger."
+1. HTML Color Picker
 2. Rounded Corners
 3. Select CSS packages
 4. Images
--- routes/compiler.js
+++ routes/compiler.js
@@ -1,26 +1,40 @@
 var express = require('express');
-var bodyParser = require("body-parser");
-
 var router = express.Router();
-
-
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
 
 router.get('/', function(req, res, next){
   res.render('form')
 })
 
-router.post('/', function(req, res, next){
-  console.log(req.body.test);
+var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test('#ac3')
 
+router.post('/', [check(*).], function(req, res, next){
+  console.log(req.body);
+  var primary = req.body.primary
+  var secondary = req.body.secondary
+  var info = req.body.info
+  var dark = req.body.light
+  var light = req.body.dark
+
+  var fs = require('fs')
+  var uuid = require('uuid');
+  const uuidv4 = require('uuid/v4');
+
+  var root = fs.readFileSync('./framework/scss/theme.scss', 'utf-8')
+  var vars = fs.readFileSync('./framework/scss/_a-global-variables.scss', 'utf-8')
+  var customRootName = uuidv4()
+  var customVarsName = uuidv4()
+  var customRoot = fs.writeFileSync('./framework/scss/'+ customRootName +'.scss', root.replace("a-global-variables.scss", customVarsName))
+  var customVarsData = vars.replace("181A7B", primary).replace("3B6CF6", secondary).replace("A513B6", info).replace("0A1438", dark).replace("e6ecf7", light)
+  var customVars = fs.writeFileSync('./framework/scss/'+ customVarsName +'.scss', customVarsData)
   var sass = require('node-sass')
   sass.render({
-    file: './framework/scss/theme.scss',
-    outputStyle: 'compressed'
+    file: './framework/scss/'+ customRootName +'.scss',
+    outputStyle: 'compressed',
   }, function(err, result) {
-    console.log(err)
+    console.log("err: " + err)
     res.render('compiler', {title: "Compiled CSS", css: result.css.toString()})
+    fs.unlinkSync('./framework/scss/'+ customRootName +'.scss')
+    fs.unlinkSync('./framework/scss/'+ customVarsName +'.scss')
   });
 })
 
