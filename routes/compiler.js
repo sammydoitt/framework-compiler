e24f7926d45bce2c3e49cd3c0b17a2ccd07c1a4c
Sammy Steiner <sasteiner@doitt.nyc.gov>
2019-08-08 11:27:16 -0400

    removed jade and added handlebars, playing with forms and post requests.

--- routes/compiler.js
+++ routes/compiler.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+
 var router = express.Router();
 
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
 router.get('/', function(req, res, next){
+  res.render('form')
+})
+
+router.post('/', function(req, res, next){
+  console.log(req.body.test);
+
   var sass = require('node-sass')
   sass.render({
     file: './framework/scss/theme.scss',
