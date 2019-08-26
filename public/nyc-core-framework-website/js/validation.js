d1ab2cf92e207a90c2391c7a77de5608f9db61a6
Sammy Steiner <sasteiner@doitt.nyc.gov>
2019-08-26 12:41:49 -0400

    added form validation with core-framework validation.

--- public/nyc-core-framework-website/js/validation.js
+++ public/nyc-core-framework-website/js/validation.js
@@ -7,20 +7,20 @@ $(document).ready(function () {
             var forms = document.getElementsByClassName('needs-validation');
 
             var validation = Array.prototype.filter.call(forms, function (form) {
-                
+
                 form.addEventListener('submit', function (event) {
 
                     if (form.checkValidity() === false) {
 
                         event.preventDefault();
                         event.stopPropagation();
-                        
+
                         var invalid_target = $(':invalid');
                         invalid_target.closest('.form-group').addClass('is-invalid');
-                        
+
                         var alert_target = $('.is-invalid:first');
                         alert_target.removeClass('d-none');
-                                
+
                         $('html, body').animate({
                             scrollTop: alert_target.offset().top - 16
                         }, {
@@ -38,7 +38,7 @@ $(document).ready(function () {
             });
         }, false);
     })();
-    
+
     $('.form-control').blur(function () {
         if ( !$(this).is(":invalid") ) {
             $(this).closest('.form-group').removeClass('is-invalid');
@@ -46,7 +46,7 @@ $(document).ready(function () {
             $(this).closest('.form-group').addClass('is-invalid');
         }
     });
-    
+
     $('.custom-control-input').change(function () {
         if (!$(this).is(":invalid")) {
             $(this).closest('.form-group').removeClass('is-invalid');
